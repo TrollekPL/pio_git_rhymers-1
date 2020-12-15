aed382cf90b36c1cb9c31ae5ab52bfc548acb654
Tomasz Zaremba <zaremba52@gmail.com>
2020-12-15 21:20:16 +0100

    1.3.2 błędy formatowania, uregulowanie wcięć

--- src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
@@ -1,34 +1,42 @@
 package edu.kis.vh.nursery;
 
-public class defaultCountingOutRhymer {
+// Alt + strzałki powoduje zmianę karty aktualnie edytowanego kodu
 
-	private int[] NUMBERS = new int[12];
+public class defaultCountingOutRhymer
+{
 
-	public int total = -1;
+    private int[] NUMBERS = new int[12];
 
-	public void countIn(int in) {
-		if (!isFull())
-			NUMBERS[++total] = in;
-	}
+    public int total = -1;
 
-		public boolean callCheck() {
-			return total == -1;
-		}
-		
-			public boolean isFull() {
-				return total == 11;
-			}
-		
-				protected int peekaboo() {
-					if (callCheck())
-						return -1;
-					return NUMBERS[total];
-				}
-			
-					public int countOut() {
-						if (callCheck())
-							return -1;
-						return NUMBERS[total--];
-					}
+    public void countIn(int in)
+    {
+        if (!isFull())
+            NUMBERS[++total] = in;
+    }
+
+    public boolean callCheck()
+    {
+        return total == -1;
+    }
+
+    public boolean isFull()
+    {
+        return total == 11;
+    }
+
+    protected int peekaboo()
+    {
+        if (callCheck())
+            return -1;
+        return NUMBERS[total];
+    }
+
+    public int countOut()
+    {
+        if (callCheck())
+            return -1;
+        return NUMBERS[total--];
+    }
 
 }
